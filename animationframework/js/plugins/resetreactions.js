0fcda01c2af2754b9f096fd99aad61e95b7ef7b4
Dushan Wegner <dushan@mehreinfach.de>
2013-04-11 13:44:04 +0200

    reset reactions updated

--- animationframework/js/plugins/resetreactions.js
+++ animationframework/js/plugins/resetreactions.js
@@ -1,6 +1,7 @@
 Actor.prototype.triggerResetReactions = function(triggeredByAction, reactionTargetIndex){
   // not intended for human consumption!
   this.reactionTargets[reactionTargetIndex].resetReactions();
+  this.reactionTargets[reactionTargetIndex].removeBehaviorsThatCameFromReacts();
 };
 
 Actor.prototype.letsResetReactions = function(targetObject, appearLength, startVisible){
