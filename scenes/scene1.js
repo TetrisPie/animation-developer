184567c7f87c69c1fc3ee9976f9a67df88de6ae8
Dushan Wegner <dushan@mehreinfach.de>
2013-05-09 18:21:38 +0200

    fixed text in scene1

--- scenes/scene1.js
+++ scenes/scene1.js
@@ -14,16 +14,16 @@ var scene1 = function(){
 
   var actor2 = scene.createActor('actor.png', 20, 270);
   actor2.movesToOnTouch(500, 270, 2, 2);
-  scene.write(20, 365, "actor2 and actor3 moves to new position on touch.");
+  scene.write(20, 365, "actor2 moves to new position on touch.");
 
-  var actor4 = scene.createActor('actor.png', 20, 415);
-  var actor5 = scene.createActor('actor.png', 300, 415, 75, 75);
-  var actor6 = scene.createActor('actor.png', 500, 415, 75, 75);
+  var actor3 = scene.createActor('actor.png', 20, 415);
+  var actor4 = scene.createActor('actor.png', 300, 415, 75, 75);
+  var actor5 = scene.createActor('actor.png', 500, 415, 75, 75);
 
-  actor4.letsMoveTo(actor5, 500, 415, 1.5, 5, 1);
-  actor4.letsMoveTo(actor6, 600, 415, 0.75, 2, 0);
+  actor3.letsMoveTo(actor4, 500, 415, 1.5, 5, 1);
+  actor3.letsMoveTo(actor5, 600, 415, 0.75, 2, 0);
 
-  scene.write(20, 510, "actor4 lets actor6 and actor7 move to new position.");
+  scene.write(20, 510, "actor3 lets actor5 and actor7 move to new position.");
 
   return scene;
 };
