c620c53cc373b93459d35eb49f04194bbc7799fb
Dushan Wegner <dushan@mehreinfach.de>
2013-05-10 08:56:30 +0200

    passing JSHint

--- animationframework/js/core/reaction.js
+++ animationframework/js/core/reaction.js
@@ -1,3 +1,5 @@
+/* jshint -W061 */
+
 var Reaction = function(actor, reactioncode, reactHowOften){
   this.actor = actor;
   this.reactioncode = reactioncode;
@@ -6,15 +8,14 @@ var Reaction = function(actor, reactioncode, reactHowOften){
 
   if (notNumeric(this.reactHowOften) || this.reactHowOften < 0) {
     this.reactHowOften = 0;
-  };
+  }
 
   this.reset = function(){
     if (this.runs > 0) {
       this.actor.image.style.cursor = 'pointer';
       this.runs = 0;
-    };
+    }
   };
-
   return this;
 };
 
@@ -22,20 +23,19 @@ Actor.prototype.react = function(){
   for (var i = 0; i < this.reactions.length; i++) {
     var myReaction = this.reactions[i];
 
-    if ((myReaction.reactHowOften == 0) || (myReaction.runs < myReaction.reactHowOften)) {
+    if ((myReaction.reactHowOften === 0) || (myReaction.runs < myReaction.reactHowOften)) {
 
-      // l(myReaction.reactioncode.replace('reactionTargetIndex', i));   
-      eval(myReaction.reactioncode.replace('reactionTargetIndex', i));   
+      eval(myReaction.reactioncode.replace('reactionTargetIndex', i));
 
       // update reaction counter:
       myReaction.runs++;
 
-      // if this will be run no more, set cursor to 
+      // if this will be run no more, set cursor to
       if ((myReaction.reactHowOften > 0) && (myReaction.runs >= myReaction.reactHowOften)) {
         this.image.style.cursor = 'auto';
-      };
-    };
-  };
+      }
+    }
+  }
 };
 
 // The following are called via plugin/script:
