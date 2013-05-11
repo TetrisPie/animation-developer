de23e004801d69b4de4540008c1086e993f5d9d6
Norman Philipp <solddrem@t-online.de>
2013-05-11 15:52:24 +0200

    tst

---
+++ animationframework/js/plugins/videoplay.js
@@ -0,0 +1,91 @@
+/* global Plugin, Actor, floatValueOfOr */ 
+
+function PlayVideo(actor, startAfter, startsAt, playLength, triggeredByAction, reactionTargetIndex){
+  var playing = new Plugin(0,0, actor, triggeredByAction, reactionTargetIndex);
+
+  playing.originalPlayTime = actor.image.currentTime * 1000;
+  playing.initVideo = true;
+  playing.newPlayTime = startsAt;
+  playing.playLength = playLength;
+
+  if (typeof startsAt == 'undefined') {
+    playing.startsAtBegin = true;
+  } else {
+    playing.startsAtBegin = false;
+  }
+
+  playing.cleanup = function () {
+      this.cleanupPlugin();
+      this.targetObject.image.pause();
+      if (this.startsAtBegin) {
+          this.targetObject.image.currentTime = 0;
+      } else {
+          this.targetObject.image.currentTime = playing.newPlayTime/1000;
+      }
+  };
+
+  playing.reset = function () {
+      playing.initVideo = true;
+      this.resetPlugin(); // quasi "call to super";
+  };
+  playing.reset();
+
+  playing.applybehavior = function () {
+      if (!this.done) {
+          alert(playing.initVideo);
+          if (playing.initVideo) {
+              alert(this.targetObject.image.currentSrc);
+              if (this.targetObject.image.currentSrc != '') {
+                  alert("dd");
+                  if (this.startsAtBegin) {
+                      this.targetObject.image.currentTime = 0;
+                  } else {
+                      this.targetObject.image.currentTime = playing.newPlayTime / 1000;
+                  }
+                  playing.initVideo = false;
+              }
+          }
+      }
+
+      if (!this.done && (this.targetObject.age() > startAfter)) {
+          this.targetObject.image.play();
+          this.isDoneWhen(true);
+      }
+  };
+  return playing;
+}
+
+Actor.prototype.playvideo = function (startAfter, startsAt, playLength, triggeredByAction, reactionTargetIndex) {
+    var behavior = new PlayVideo(this, startAfter, startsAt, playLength, triggeredByAction, reactionTargetIndex);
+    if (typeof triggeredByAction !== 'undefined') {
+        behavior.triggeredByAction = triggeredByAction;
+    }
+    this.addBehavior(behavior);
+    return this;
+};
+
+/*
+
+Actor.prototype.playvideoOnTouch = function(appearLength){
+  this.reacts("this.playvideo(0," + appearLength + ", true, true, reactionTargetIndex);", 1);
+  return this;
+};
+
+Actor.prototype.resumevideoOnTouch = function(appearLength){
+  this.reacts("this.playvideo(0," + appearLength + ", true, false, reactionTargetIndex);", 1);
+  return this;
+};
+
+
+Actor.prototype.letsAppearStartsVisible = function(targetObject, appearLength){
+  this.reacts("this.appears(0, " + floatValueOfOr(appearLength, 1000) + ", true, true, reactionTargetIndex);", 1, targetObject);
+  return this;
+};
+
+Actor.prototype.letsAppear = function(targetObject, appearLength){
+  targetObject.setInvisible();
+  this.reacts("this.appears(0, " + floatValueOfOr(appearLength, 1000) + ", false, true, reactionTargetIndex);", 1, targetObject);
+  return this;
+};
+
+*/
--- scenes/scene25.js
+++ scenes/scene25.js
@@ -8,14 +8,13 @@ var scene25 = function () {
     home.navigatesOnTouch('scene0');
 
     var actor1 = scene.createVideoActor('Chrome_ImF', 200, 100, 240, 88);
-    actor1.image.setAttribute("controls", "controls");
-    actor1.resets(500);
+    actor1.playvideo(0, 5000);
     scene.write(20, 205, "videoactor1 contains without playing it.");
 
-    var actor2 = scene.createVideoActor('Chrome_ImF', 100, 200, 240, 88);
+   /* var actor2 = scene.createVideoActor('Chrome_ImF', 100, 200, 240, 88);
     actor2.image.setAttribute("controls", "controls");
     actor2.movesToOnTouch(500, 270, 2, 2);
     scene.write(20, 365, "videoactor2 moves when touched diagonally to new position without playing.");
-
+    */
     return scene;
 };
