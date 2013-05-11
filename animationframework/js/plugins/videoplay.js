7a761267175582072835b59a42338b24a7a3a84f
Norman Philipp <solddrem@t-online.de>
2013-05-11 23:47:06 +0200

    letsplay angefangen

--- animationframework/js/plugins/videoplay.js
+++ animationframework/js/plugins/videoplay.js
@@ -24,33 +24,35 @@ function PlayVideo(actor, startAfter, startsAt, playLength, triggeredByAction, r
   };
 
 
-  playing.listener = function(){
-          playing.reset = function () {
-            if (playing.videoReady && (this.targetObject.image.currentSrc != '')) {
-                if (this.startsAtBegin) {
-                        this.targetObject.image.currentTime = 0;
-                    } else {
-                        this.targetObject.image.currentTime = playing.newPlayTime / 1000;
-                    }
-                }
-                this.resetPlugin(); // quasi "call to super";
-            };
-            playing.reset();
+  playing.completeInit = function () {
+      playing.reset = function () {
+          if (playing.videoReady && (this.targetObject.image.currentSrc != '')) {
+              if (this.startsAtBegin) {
+                  this.targetObject.image.currentTime = 0;
+              } else {
+                  this.targetObject.image.currentTime = playing.newPlayTime / 1000;
+              }
+          }
+          this.resetPlugin(); // quasi "call to super";
+      };
+      playing.reset();
 
-            playing.applybehavior = function () {
-                if (!this.done && (this.targetObject.age() > startAfter)) {
-                    this.targetObject.image.play();
-                    this.isDoneWhen(true)
-                }
-            };
-       this.targetObject.image.removeEventListener('click', playing.listener , false);
-    };
+      playing.applybehavior = function () {
+          if (!this.done && (this.targetObject.age() > startAfter)) {
+              this.targetObject.image.play();
+              this.isDoneWhen(true)
+          }
+      };
+      actor.image.removeEventListener('canplay', playing.completeInit, false);
+  };
 
  if (actor.image.readyState != 4) {
-     actor.image.addEventListener('canplay', playing.listener ,false);
+     actor.image.addEventListener('canplay', playing.completeInit ,false);
+     playing.applybehavior = function () { };
      return playing;
      } else {
-     playing.listener();
+        playing.completeInit();
+        return playing;
      }
 }
 
@@ -63,13 +65,18 @@ Actor.prototype.playvideo = function (startAfter, startsAt, playLength, triggere
     return this;
 };
 
-/*
 
-Actor.prototype.playvideoOnTouch = function(appearLength){
-  this.reacts("this.playvideo(0," + appearLength + ", true, true, reactionTargetIndex);", 1);
+Actor.prototype.playvideoOnTouch = function(startAfter, startsAt, playLength){
+  this.reacts("this.playvideo(" + startAfter + "," + startsAt + ", " + playLength + ", true, reactionTargetIndex);", 1);
   return this;
 };
 
+Actor.prototype.letsPlayvideo = function(targetObject, startAfter, startsAt, playLength){
+  this.reacts("this.playvideo(" + startAfter + "," + startsAt + ", " + playLength + ", true, reactionTargetIndex);", 1, targetObject);
+  return this;
+};
+
+/*
 Actor.prototype.resumevideoOnTouch = function(appearLength){
   this.reacts("this.playvideo(0," + appearLength + ", true, false, reactionTargetIndex);", 1);
   return this;
--- scenes/scene25.js
+++ scenes/scene25.js
@@ -9,11 +9,12 @@ var scene25 = function () {
 
     var actor1 = scene.createVideoActor('Chrome_ImF', 200, 100, 240, 88);
     actor1.playvideo(0, 5000, 5000);
+    actor1.letsPlayvideo(actor2, 0, 5000, 5000);
     scene.write(20, 205, "videoactor1 contains without playing it.");
 
     var actor2 = scene.createVideoActor('Chrome_ImF', 100, 200, 240, 88);
     actor2.movesToOnTouch(500, 270, 2, 2);
-    actor2.playvideo(2000, 1000, 2000);
+    actor2.playvideoOnTouch(2000, 1000, 2000);
     scene.write(20, 365, "videoactor2 moves when touched diagonally to new position without playing.");
     
     return scene;
