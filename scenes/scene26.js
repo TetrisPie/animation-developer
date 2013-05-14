1f69d29275beebb0838aeedb1e070aef00b37f24
Dushan Wegner <dushan@mehreinfach.de>
2013-05-14 19:02:20 +0200

    pretty demo for colognejs

---
+++ scenes/scene26.js
@@ -0,0 +1,30 @@
+/* global Scene */
+/* exported scene25 */
+
+var scene26 = function () {
+  var scene = new Scene('scene26', 'Video');
+
+  var home = scene.createActor('home.png', 10, 10);
+  home.navigatesOnTouch('scene0');
+
+  var planet = scene.createActor('planeten/erde.png',150,190,500,500);
+  planet.rotates(-0.05);
+
+  var man = scene.createActor('walkcycle/01walk.png', 350, 100, 80, 150);
+  man.addPhase('walkcycle/02walk.png');
+  man.addPhase('walkcycle/03walk.png');
+  man.addPhase('walkcycle/04walk.png');
+  man.addPhase('walkcycle/05walk.png');
+  man.addPhase('walkcycle/06walk.png');
+  man.addPhase('walkcycle/07walk.png');
+  man.addPhase('walkcycle/08walk.png');
+  man.addPhase('walkcycle/09walk.png');
+  man.addPhase('walkcycle/10walk.png');
+  man.addPhase('walkcycle/11walk.png');
+  man.addPhase('walkcycle/12walk.png');
+  man.phaseCycle = 1400;
+
+  man.bouncesOnTouch(100,10,5,0.6);
+
+  return scene;
+};
