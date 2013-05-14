9d64e91f41d2f4983f349dc031459258a52eb10c
Norman Philipp <solddrem@t-online.de>
2013-05-14 11:37:55 +0200

    Controls bei mobilen devices einblenden

--- animationframework/js/plugins/videoactor.js
+++ animationframework/js/plugins/videoactor.js
@@ -6,8 +6,13 @@ Scene.prototype.createVideoActor = function (fileName, startAtX, startAtY, width
         actor.image = document.createElement('video');
         actor.image.originalPath = fileName;
         actor.filename = fileName.substring(fileName.lastIndexOf('/') + 1)
-        //actor.image.setAttribute('poster', relativeOrAbsolutePath(defaultVideoDirectory, actor.image.originalPath) + '.png');
-                           
+        if (ismobile()) {
+            actor.image.setAttribute('controls', 'controls');
+            //actor.image.setAttribute('poster', relativeOrAbsolutePath(defaultVideoDirectory, actor.image.originalPath) + '.png');
+        }
+
+        console.log(relativeOrAbsolutePath(defaultVideoDirectory, actor.image.originalPath));
+
         var srcTag = actor.image.appendChild(document.createElement('source'));
         srcTag.setAttribute('type', 'video/mp4');
         srcTag.setAttribute('src', relativeOrAbsolutePath(defaultVideoDirectory, actor.image.originalPath) + '.mp4');
@@ -19,7 +24,6 @@ Scene.prototype.createVideoActor = function (fileName, startAtX, startAtY, width
         mediaType = 'video';
 
         actor.image.actor = actor;
-        actor.phases.push(relativeOrAbsolutePath(defaultVideoDirectory, fileName));
         actor.defaultdirectory = defaultVideoDirectory;
         actor.position = { x: startAtX, y: startAtY };
         actor.setSize(width, height);
@@ -43,4 +47,14 @@ Scene.prototype.createVideoActor = function (fileName, startAtX, startAtY, width
     this.actors.push(actor);
     return actor;
 };
+
+function ismobile(){
+    return false
+        || (navigator.userAgent.match(/iPad/i) != null)    
+        || (navigator.userAgent.match(/iPhone/i) != null) 
+        || (navigator.userAgent.match(/iPod/i) != null)
+        || (navigator.userAgent.match(/Android/i) != null)
+        || (navigator.userAgent.match(/IEMobile/i) != null)
+    ;
+}
 
