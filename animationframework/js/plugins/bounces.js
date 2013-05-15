90d6eac79ef5799584ab7c428f2aee683ca8fe46
Dushan Wegner <dushan@mehreinfach.de>
2013-05-15 12:08:08 +0200

    removed scene 26, but kept in the sample files

--- animationframework/js/plugins/bounces.js
+++ animationframework/js/plugins/bounces.js
@@ -1,11 +1,10 @@
-
 /* Simple bouncing plugin, makes an actor bounce upward, using a sine function
 (not physically accurate, but looks more believable than a quadratic curve)
  Parameters:
  - amplitude: Height of bounce in screen units
  - speed: Bounces per second
  - times: Number of bounces, specify 0 for infinite bouncing
- - damping: Amplitude reduction (0.0-1.0) or increase (>1.0) over time. Specify 1.0 for constant amplitude. 
+ - damping: Amplitude reduction (0.0-1.0) or increase (>1.0) over time. Specify 1.0 for constant amplitude.
 */
 
 function Bouncing(actor, amplitude, speed, times, damping, triggeredByAction, reactionTargetIndex) {
@@ -26,7 +25,7 @@ function Bouncing(actor, amplitude, speed, times, damping, triggeredByAction, re
 
   bouncing.applybehavior = function(){
     var time = ((now() - this.startedAt) * this.speed / 1000);      //bounces since start of behaviour
-    var y = (this.times < 1) || (time < this.times) ?  //Bounce or not? 
+    var y = (this.times < 1) || (time < this.times) ?  //Bounce or not?
 		(-Math.abs(this.amplitude * Math.sin(time * this.speed * Math.PI)) * Math.pow(this.damping, time)) :
 	 	0;
     this.targetObject.vector.y += y - this.lastY;      //Modify position of actor (delta to last)
