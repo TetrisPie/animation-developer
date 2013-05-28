bf10321f97aea7ef5feeabde58968387a045ac7a
iDushan <dushan@mehreinfach.de>
2013-05-28 13:14:21 +0300

    converted tabs to double spaces

--- animationframework/js/core/scene.js
+++ animationframework/js/core/scene.js
@@ -2,169 +2,169 @@
 /* exported Scene */
 
 function Scene(id, title, width, height){
-	this.id = id;
-	this.title = (typeof title !== 'undefined' ? title : id);
-	this.div = createDiv(id, 'scene');
+  this.id = id;
+  this.title = (typeof title !== 'undefined' ? title : id);
+  this.div = createDiv(id, 'scene');
 
-	this.isVisible = false;
-	this.actors = [];
-	this.texts = [];
-	this.textDisplaying = true;
-	this.alwaysShowsText = false;
-	this.scrollingLocked = false;
-	this.displayedAt = Date.now();
-	scrollingPerspectiveStrength = 'undefined';
+  this.isVisible = false;
+  this.actors = [];
+  this.texts = [];
+  this.textDisplaying = true;
+  this.alwaysShowsText = false;
+  this.scrollingLocked = false;
+  this.displayedAt = Date.now();
+  scrollingPerspectiveStrength = 'undefined';
 
-	// dimensions of the scene's div and related settings -->
-	this.dimensions = {};
-	this.dimensions.x = (typeof width !== 'undefined') ? parseInt(width, 10) : 0;
-	this.dimensions.y = (typeof height !== 'undefined') ? parseInt(height, 10) : 0;
-	// <-- dimensions of the scene's div and related settings
+  // dimensions of the scene's div and related settings -->
+  this.dimensions = {};
+  this.dimensions.x = (typeof width !== 'undefined') ? parseInt(width, 10) : 0;
+  this.dimensions.y = (typeof height !== 'undefined') ? parseInt(height, 10) : 0;
+  // <-- dimensions of the scene's div and related settings
 
     this.scrollTo(1,0);
     this.scrollTo(0,0);
 
-	this.setSzeneSizeToStageIfNotSetInScenedefinition = function(newWidth, newHeight){
-		// width
-		if (this.dimensions.x === 0) this.dimensions.x = parseInt(newWidth, 10);
-		this.div.style.width = this.dimensions.x + 'px';
-		// height
-		if (this.dimensions.y === 0) this.dimensions.y = parseInt(newHeight, 10);
-		this.div.style.height = this.dimensions.y + 'px';
-	};
+  this.setSzeneSizeToStageIfNotSetInScenedefinition = function(newWidth, newHeight){
+    // width
+    if (this.dimensions.x === 0) this.dimensions.x = parseInt(newWidth, 10);
+    this.div.style.width = this.dimensions.x + 'px';
+    // height
+    if (this.dimensions.y === 0) this.dimensions.y = parseInt(newHeight, 10);
+    this.div.style.height = this.dimensions.y + 'px';
+  };
 
-	this.resetAge = function(){
-		this.displayedAt = Date.now();
-		return this.displayedAt;
-	};
+  this.resetAge = function(){
+    this.displayedAt = Date.now();
+    return this.displayedAt;
+  };
 
-	this.age = function(){
-		return Date.now() - this.displayedAt;
-	};
+  this.age = function(){
+    return Date.now() - this.displayedAt;
+  };
 
-	this.alwaysShowText = function(){
-		this.alwaysShowsText = true;
-	};
+  this.alwaysShowText = function(){
+    this.alwaysShowsText = true;
+  };
 
-	// scene itself should preload as minimum,
-	// further preload-scenes are added by the navigates-plugin
-	this.preloadSceneIds = [id];
+  // scene itself should preload as minimum,
+  // further preload-scenes are added by the navigates-plugin
+  this.preloadSceneIds = [id];
 
-	this.resetActors = function(){
-		for (var i = this.actors.length - 1; i >= 0; i--) {
-			this.actors[i].reset();
-		}
-	};
+  this.resetActors = function(){
+    for (var i = this.actors.length - 1; i >= 0; i--) {
+      this.actors[i].reset();
+    }
+  };
 
-	this.removeBehaviorsThatCameFromReacts = function(){
-		for (var i = this.actors.length - 1; i >= 0; i--) {
-			try {
-				this.actors[i].removeBehaviorsThatCameFromReacts();
-			} catch(e){}
-		}
-	};
+  this.removeBehaviorsThatCameFromReacts = function(){
+    for (var i = this.actors.length - 1; i >= 0; i--) {
+      try {
+        this.actors[i].removeBehaviorsThatCameFromReacts();
+      } catch(e){}
+    }
+  };
 
-	this.makeVisible = function () {
-		this.div.style.display = 'block';
-		this.isVisible = true;
-		this.scrollingLocked = false;
-		this.setScrolling();
-	};
+  this.makeVisible = function () {
+    this.div.style.display = 'block';
+    this.isVisible = true;
+    this.scrollingLocked = false;
+    this.setScrolling();
+  };
 
-	this.makeInvisible = function(){
-		this.div.style.display = 'none';
-		this.isInvisible = false;
-		this.resetActors(); // while we are at it
-		this.removeBehaviorsThatCameFromReacts();
-	};
-	this.makeInvisible(); // start out invisible
+  this.makeInvisible = function(){
+    this.div.style.display = 'none';
+    this.isInvisible = false;
+    this.resetActors(); // while we are at it
+    this.removeBehaviorsThatCameFromReacts();
+  };
+  this.makeInvisible(); // start out invisible
 
-	this.cleanup = function () {
+  this.cleanup = function () {
     for (var i = this.actors.length - 1; i >= 0; i--) {
       this.actors[i].cleanupBehaviors();
     }
     this.scrollingLocked = false;
-	};
+  };
 
-	this.makeOthersInvisible = function(){
-		var showTextInCurrent = window.animation.textIsDisplaying;
+  this.makeOthersInvisible = function(){
+    var showTextInCurrent = window.animation.textIsDisplaying;
 
-		for (var i = window.animation.loadedScenes.length - 1; i >= 0; i--) {
-			if (window.animation.loadedScenes[i].id.match(this.id)) {
-				this.makeVisible();
-			} else {
-				window.animation.loadedScenes[i].makeInvisible();
-				window.animation.loadedScenes[i].hideText();
-			}
-		}
+    for (var i = window.animation.loadedScenes.length - 1; i >= 0; i--) {
+      if (window.animation.loadedScenes[i].id.match(this.id)) {
+        this.makeVisible();
+      } else {
+        window.animation.loadedScenes[i].makeInvisible();
+        window.animation.loadedScenes[i].hideText();
+      }
+    }
 
-		if (showTextInCurrent || this.alwaysShowsText) {
-			this.showText();
-		} else {
-			this.hideText();
-		}
+    if (showTextInCurrent || this.alwaysShowsText) {
+      this.showText();
+    } else {
+      this.hideText();
+    }
 
-		window.animation.textIsDisplaying = showTextInCurrent;
-	};
+    window.animation.textIsDisplaying = showTextInCurrent;
+  };
 
-	this.mute = function(){
-		for (var i = 0; i < this.actors.length; i++) {
-			try {
-				this.actors[i].pauseAudio();
-			} catch(e){}
-		}
-	};
+  this.mute = function(){
+    for (var i = 0; i < this.actors.length; i++) {
+      try {
+        this.actors[i].pauseAudio();
+      } catch(e){}
+    }
+  };
 
-	this.muteOthers = function(){
-		for (var i = window.animation.loadedScenes.length - 1; i >= 0; i--) {
-			if (!window.animation.loadedScenes[i].id.match(this.id)) {
-				window.animation.loadedScenes[i].mute();
-			}
-		}
-	};
+  this.muteOthers = function(){
+    for (var i = window.animation.loadedScenes.length - 1; i >= 0; i--) {
+      if (!window.animation.loadedScenes[i].id.match(this.id)) {
+        window.animation.loadedScenes[i].mute();
+      }
+    }
+  };
 
-	this.enterActors = function(){
-		for (var i = 0; i < this.actors.length; i++) {
-			this.actors[i].enter(this);
-		}
-	};
+  this.enterActors = function(){
+    for (var i = 0; i < this.actors.length; i++) {
+      this.actors[i].enter(this);
+    }
+  };
 
-	this.write = function(myX, myY, html, cssclass){
-		var newText = document.createElement('div');
-		newText.innerHTML = html;
-		newText.setAttribute('class', 'text ' + (typeof cssclass === "undefined" ? "" : cssclass));
-		newText.style.left = myX + 'px';
-		newText.style.top = myY + 'px';
-		this.div.appendChild(newText);
-		this.texts.push(newText);
-	};
+  this.write = function(myX, myY, html, cssclass){
+    var newText = document.createElement('div');
+    newText.innerHTML = html;
+    newText.setAttribute('class', 'text ' + (typeof cssclass === "undefined" ? "" : cssclass));
+    newText.style.left = myX + 'px';
+    newText.style.top = myY + 'px';
+    this.div.appendChild(newText);
+    this.texts.push(newText);
+  };
 
-	this.showText = function(){
-		this.textDisplaying = true;
-		for (var i = this.texts.length - 1; i >= 0; i--) {
-			this.texts[i].style.visibility = 'visible';
-		}
-		window.animation.textIsDisplaying = true;
-	};
+  this.showText = function(){
+    this.textDisplaying = true;
+    for (var i = this.texts.length - 1; i >= 0; i--) {
+      this.texts[i].style.visibility = 'visible';
+    }
+    window.animation.textIsDisplaying = true;
+  };
 
-	this.hideText = function(){
-		this.textDisplaying = false;
-		for (var i = this.texts.length - 1; i >= 0; i--) {
-			this.texts[i].style.visibility = 'hidden';
-		}
-		window.animation.textIsDisplaying = false;
-	};
+  this.hideText = function(){
+    this.textDisplaying = false;
+    for (var i = this.texts.length - 1; i >= 0; i--) {
+      this.texts[i].style.visibility = 'hidden';
+    }
+    window.animation.textIsDisplaying = false;
+  };
 
-	return this;
+  return this;
 }
 
 Scene.prototype.scrollTo = function (x, y) {
   if (typeof x != 'undefined') {
     window.animation.stageDiv.scrollLeft = x;
   }
-	if (typeof y != 'undefined') {
-    window.animation.stageDiv.scrollTop = y;
-  }
+  if (typeof y != 'undefined') {
+      window.animation.stageDiv.scrollTop = y;
+    }
 };
 
 Scene.prototype.lockScrolling = function () {
@@ -178,20 +178,20 @@ Scene.prototype.unlockScrolling = function () {
 };
 
 Scene.prototype.setScrolling = function () {
-	if ((this.dimensions.x > window.animation.width) && !(this.scrollingLocked)) {
+  if ((this.dimensions.x > window.animation.width) && !(this.scrollingLocked)) {
     console.log("a");   
     window.animation.stageDiv.style.overflowX = "scroll";
-	} else {
+  } else {
     console.log("b");
     window.animation.stageDiv.style.overflowX = "hidden";
-	}
+  }
 
-	if ((this.dimensions.y > window.animation.height) && !(this.scrollingLocked)) {
+  if ((this.dimensions.y > window.animation.height) && !(this.scrollingLocked)) {
     console.log("a");
     window.animation.stageDiv.style.overflowY = "scroll";
-	} else {
+  } else {
     console.log("b");
     window.animation.stageDiv.style.overflowY = "hidden";
-	}
+  }
 };
 
